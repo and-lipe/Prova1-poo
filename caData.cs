57b13b980fb44d6a9dedccd69a7518958c9d0513
André Felipe <51057452+and-lipe@users.noreply.github.com>
2021-08-30 10:21:41 -0300

    Add files via upload

---
+++ caData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace caData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data t0 = new Data();
+            Data t1 = new Data(03,06,2020);
+            Data t2 = new Data(27, 04, 2021);
+
+            Console.WriteLine("\nComparando t2 com t1:\t" + t1.Compara(t2));
+            
+
+            Console.WriteLine("\nMes Extenso para t1 e t2 respectivamente: " + t1.getMesExtenso() + " " + t2.getMesExtenso());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("\nt1 é bissexto?\t" + t1.isBissexto());
+            
+            Console.WriteLine("\nt2 é bissexto?\t" + t2.isBissexto());
+
+            Console.WriteLine("\nColando t1\n");
+            t0 = t1.clone();
+            Console.WriteLine("\nt0= " + t0.getDia() + "/" + t0.getMes() + "/"+t0.getAno());
+        }
+
+    }
+}
---
+++ clData.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caData
+{
+    class Data
+    {
+        //Atributos
+        private int dia;
+        private int mes;
+        private int ano;
+
+        //Construtores
+        public Data()
+        {
+            dia = 0;
+            mes = 0;
+            ano = 0;
+        }
+
+        public Data(int _D, int _M, int _A)
+        {
+            dia = _D;
+            mes = _M;
+            ano = _A;
+        }
+
+        //getters
+        public int getDia()
+        {
+            return dia;
+        }
+
+        public int getMes()
+        {
+            return mes;
+        }
+
+        public string getMesExtenso()
+        {
+            string[] meses = new string[] {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
+            return meses[mes-1];
+        }
+
+        public int getAno()
+        {
+            return ano;
+        }
+
+        //Setters
+        public void setDia(int _d)
+        {
+            dia = _d;
+        }
+        public void setMes(int _m)
+        {
+            mes = _m;
+        }
+        public void setAno(int _a)
+        {
+            ano = _a;
+        }
+
+        public int Compara (Data t)
+        {
+            if (this.ano == t.ano & this.mes == t.mes & this.dia == t.dia)
+            {
+                return 0;
+            }
+
+            if (this.ano >= t.ano & this.mes >= t.mes & this.dia >= t.dia)
+            {
+                return 1;
+            }
+
+            else return -1;
+        } 
+
+        public Boolean isBissexto()
+        {
+            if ((this.ano % 4) == 0)
+            {
+                if ((this.ano % 400) == 0)
+                {
+                    return true;
+                }
+                if ((this.ano % 100) != 0)
+                {
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+        }
+
+        public Data clone()
+        {
+            Data t = new Data(this.dia, this.mes, this.ano);
+            return t;
+        }
+    }
+}
